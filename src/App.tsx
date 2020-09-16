d740d2e6e6b3f752e1dbb59b187a0486b118ca40
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-09-16 21:39:01 +0530

    Added Basic Files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Settings from './ProfilePic/ProfilePic';
+
+function App() {
+  return (
+    <Settings></Settings>
+  );
+}
+
+export default App;
